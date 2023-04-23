d6c455ce69be4aac6cc4782f91eb78569b97555e
s2137127 <s2137127@gmail.com>
2023-04-23 23:19:44 +0800

    4/23

---
+++ hw3/src/part4.py
@@ -0,0 +1,108 @@
+import numpy as np
+import cv2
+import random
+from tqdm import tqdm
+from utils import solve_homography, warping
+
+
+np.random.seed(999)
+
+
+def Ransac(kp1, kp2, matches, n=4, th=500):
+
+    matches = np.array([[matches[i].queryIdx, matches[i].trainIdx] for i in range(len(matches))])
+    print('matches', matches.shape)
+    p = 0.5
+    P = 0.999
+    k = np.ceil(np.log(1 - P) / np.log(1 - p ** n)).astype(int)
+    H_arr, inlinear,inlinear_arr = [], [],[]
+    for t in range(k):
+        idx = np.random.randint(low=0, high=matches.shape[0], size=n,)
+        id1, id2 = matches[idx, 0], matches[idx, 1]
+        # for i in range(matches.shape[0]):
+        #     a = kp1[matches[i, 0]]
+        #     print(a)
+        p1, p2 = np.array([list(kp1[matches[i, 0]]) + [1] for i in range(matches.shape[0]) if i not in idx]), \
+                 np.array([list(kp2[matches[i, 1]]) + [1] for i in range(matches.shape[0]) if i not in idx])
+        # print(p1)
+        H = solve_homography(kp1[id1], kp2[id2])
+        # print(H)
+        p2_pred = np.dot(H, p1.T)
+
+        err = np.subtract(p2_pred, p2.T)
+        # print(err.shape)
+        err = np.sqrt(np.square(err[0, :]) + np.square(err[1, :]))
+        # print(np.where(err < th))
+        inlinear_arr.append(np.where(err < th))
+        inlinear.append(np.where(err < th)[0].shape[0])
+        H_arr.append(H)
+    idx = np.argmax(inlinear)
+    print(H_arr[idx])
+    return H_arr[idx],inlinear_arr[idx][0]
+
+
+def panorama(imgs):
+    """
+    Image stitching with estimated homograpy between consecutive
+    :param imgs: list of images to be stitched
+    :return: stitched panorama
+    """
+    h_max = max([x.shape[0] for x in imgs])
+    w_max = sum([x.shape[1] for x in imgs])
+
+    # create the final stitched canvas
+    dst = np.zeros((h_max, w_max, imgs[0].shape[2]), dtype=np.uint8)
+    dst[:imgs[0].shape[0], :imgs[0].shape[1]] = imgs[0]
+    last_best_H = np.eye(3)
+    out = None
+    orb = cv2.ORB_create(nfeatures=4000)
+    # for all images to be stitched:
+    w = imgs[0].shape[1]
+    for idx in tqdm(range(len(imgs) - 1)):
+        im1 = imgs[idx]
+        im2 = imgs[idx + 1]
+        im1_g = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
+        im2_g = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
+
+        # TODO: 1.feature detection & matching
+        kp1, f1 = orb.detectAndCompute(im1_g, None)
+        kp2, f2 = orb.detectAndCompute(im2_g, None)
+
+
+        # print(kp2.shape)
+        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+        matches = bf.match(f1, f2)
+        matches = sorted(matches, key=lambda x: x.distance)[:40]
+        pic = cv2.drawMatches(im1, kp1, im2, kp2, matches,None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+        cv2.imshow('ss', cv2.resize(pic,(800,800)))
+        cv2.waitKey()
+        kp1 = np.array([i.pt for i in kp1]).astype(int)
+        kp2 = np.array([i.pt for i in kp2]).astype(int)
+        # TODO: 2. apply RANSAC to choose best H
+        H,inlinear = Ransac(kp2, kp1, matches)
+        # print(inlinear)
+        m = np.array(matches)[inlinear]
+        idx = [i.queryIdx for i in m]
+        # print(idx)
+        # TODO: 3. chain the homographies
+        last_best_H = np.dot(last_best_H , H)
+        # TODO: 4. apply warping
+        kp = kp1[idx]
+
+        # print(last_best_H)
+        out = warping(im2,dst, last_best_H, np.min(kp[:,0]),im2.shape[0],np.min(kp[:,1]),w_max,
+                      direction='b')
+        cv2.imshow('ss', cv2.resize(out, (800, 800)))
+        cv2.waitKey()
+        w += im2.shape[1]
+    return out
+
+
+if __name__ == "__main__":
+    # ================== Part 4: Panorama ========================
+    # TODO: change the number of frames to be stitched
+    FRAME_NUM = 3
+    imgs = [cv2.imread('../resource/frame{:d}.jpg'.format(x)) for x in range(1, FRAME_NUM + 1)]
+    output4 = panorama(imgs)
+    cv2.imwrite('output4.png', output4)
---
+++ hw3/src/utils.py
@@ -0,0 +1,143 @@
+import numpy as np
+
+
+def solve_homography(u, v):
+    """
+    This function should return a 3-by-3 homography matrix,
+    u, v are N-by-2 matrices, representing N corresponding points for v = T(u)
+    :param u: N-by-2 source pixel location matrices
+    :param v: N-by-2 destination pixel location matrices
+    :return:
+    """
+    N = u.shape[0]
+
+    if v.shape[0] is not N:
+        print('u and v should have the same size')
+        return None
+    if N < 4:
+        print('At least 4 points should be given')
+
+    # TODO: 1.forming A
+    A = None
+    for i in range(N):
+        ui = u[i]
+        vi = v[i]
+        tmp = np.array([[ui[0], ui[1], 1, 0, 0, 0, -vi[0] * ui[0], -vi[0] * ui[1]], \
+               [0, 0, 0, ui[0], ui[1], 1, -vi[1] * ui[0], -vi[1] * ui[1]]])
+        if i == 0:
+            A = tmp
+            # A = A[:,np.newaxis]
+        else:
+            A = np.concatenate((A,tmp),axis=0)
+
+    A = np.array(A)
+    b = np.array(v).reshape(-1,1)
+    # TODO: 2.solve H with A
+    if (np.linalg.det(A)):
+
+        H = np.dot(np.linalg.inv(A), b)
+    else:
+        H = np.dot(np.linalg.pinv(A), b)
+    H = np.append(H,1).reshape(3,3)
+    # print(H)
+    return H
+
+
+def warping(src, dst, H, ymin, ymax, xmin, xmax, direction='b'):
+    """
+    Perform forward/backward warpping without for loops. i.e.
+    for all pixels in src(xmin~xmax, ymin~ymax),  warp to destination
+          (xmin=0,ymin=0)  source                       destination
+                         |--------|              |------------------------|
+                         |        |              |                        |
+                         |        |     warp     |                        |
+    forward warp         |        |  --------->  |                        |
+                         |        |              |                        |
+                         |--------|              |------------------------|
+                                 (xmax=w,ymax=h)
+
+    for all pixels in dst(xmin~xmax, ymin~ymax),  sample from source
+                            source                       destination
+                         |--------|              |------------------------|
+                         |        |              | (xmin,ymin)            |
+                         |        |     warp     |           |--|         |
+    backward warp        |        |  <---------  |           |__|         |
+                         |        |              |             (xmax,ymax)|
+                         |--------|              |------------------------|
+
+    :param src: source image
+    :param dst: destination output image
+    :param H:
+    :param ymin: lower vertical bound of the destination(source, if forward warp) pixel coordinate
+    :param ymax: upper vertical bound of the destination(source, if forward warp) pixel coordinate
+    :param xmin: lower horizontal bound of the destination(source, if forward warp) pixel coordinate
+    :param xmax: upper horizontal bound of the destination(source, if forward warp) pixel coordinate
+    :param direction: indicates backward warping or forward warping
+    :return: destination output image
+    """
+
+    h_src, w_src, ch = src.shape
+    h_dst, w_dst, ch = dst.shape
+    H_inv = np.linalg.inv(H)
+    # TODO: 1.meshgrid the (x,y) coordinate pairs
+    x = np.linspace(xmin, xmax,xmax-xmin)
+    y = np.linspace(ymin, ymax,ymax-ymin)
+    mesh = np.array(np.meshgrid(x,y)).reshape(2,-1)
+    mesh = np.concatenate((mesh,np.ones(shape=(1,mesh.shape[1]))),axis=0)
+    # TODO: 2.reshape the destination pixels as N x 3 homogeneous coordinate
+
+    if direction == 'b':
+        # TODO: 3.apply H_inv to the destination pixels and retrieve (u,v) pixels, then reshape to (ymax-ymin),(xmax-xmin)
+        v = np.dot(H_inv, mesh).T
+        v = np.divide(v[:, :2], np.tile(v[:, 2], (2, 1)).T)
+        v = v.reshape((ymax - ymin), (xmax - xmin), -1)
+        v = np.round(v).astype(int)
+        # print(v.shape)
+        # TODO: 4.calculate the mask of the transformed coordinate (should not exceed the boundaries of source image)
+
+        # TODO: 5.sample the source image with the masked and reshaped transformed coordinates
+        valid1 = np.logical_and(0 < v[:, :, 0], v[:, :, 0] < w_src)
+        valid2 = np.logical_and(0 < v[:, :, 1], v[:, :, 1] < h_src)
+        valid = np.logical_and(valid1, valid2)
+
+        # v[np.where( v<0)] = 0
+        # v[:,:,0][np.where( v[:, :, 0] >= w_src)] = w_src-1
+        # # print(v[np.where(v[:, :, 0] >= w_src)][..., 0])
+        # v[:,:,1][np.where( v[:, :, 1] >= h_src)] = h_src - 1
+        # TODO: 6. assign to destination image with proper masking
+        # print(w_src)
+        v = np.multiply(np.tile(valid, 2).reshape(v.shape), v)
+        # src = np.multiply(np.tile(valid, 3).reshape(src.shape), src)
+        # print(src[v[...,1],v[...,0]])
+        # print(v.any())
+        dst[ymin:ymin+v.shape[0],xmin:xmin+v.shape[1]] = src[v[...,1],v[...,0]]
+        # for i in range(v.shape[0]):
+        #     for j in range(v.shape[1]):
+        #         if valid[i, j]:
+        #             dst[i+ymin,j+xmin] = src[v[i,j,1],v[i,j,0]]
+    elif direction == 'f':
+        # TODO: 3.apply H to the source pixels and retrieve (u,v) pixels, then reshape to (ymax-ymin),(xmax-xmin)
+        v = np.dot(H,mesh).T
+        # print(np.tile(v[:,2],(2,1)))
+        v = np.divide(v[:,:2],np.tile(v[:,2],(2,1)).T)
+        v = v.reshape((ymax-ymin),(xmax-xmin),-1)
+        v= np.round(v).astype(int)
+        # TODO: 4.calculate the mask of the transformed coordinate (should not exceed the boundaries of destination image)
+        # mask_xu = np.ones(v.shape[:2])*xmax
+        # mask_xl = np.ones(v.shape[:2]) * xmin
+        # mask_yu = np.ones(v.shape[:2])*ymax
+        # mask_yl = np.ones(v.shape[:2]) * ymin
+
+        # TODO: 5.filter the valid coordinates using previous obtained mask
+        valid1 = np.logical_and(0 < v[:, :, 0] , v[:, :, 0]<w_dst)
+        valid2 = np.logical_and(0 < v[:, :, 1] , v[:, :, 1]<h_dst)
+        valid = np.logical_and(valid1,valid2)
+
+        v = np.multiply(np.tile(valid,2).reshape(v.shape),v)
+        src = np.multiply(np.tile(valid,3).reshape(src.shape),src)
+        # TODO: 6. assign to destination image using advanced array indicing
+        dst[v[..., 1], v[..., 0]] = src
+
+
+
+    return dst
